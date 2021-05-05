c8f8a32b45921d4eaeed790701337a311dd9ef49
Mariana Hiba <mariana_hiba@hotmail.com>
2021-05-05 19:12:43 -0300

    datos js

--- js/datos.js
+++ js/datos.js
@@ -17,3 +17,35 @@ class Empleado {
         this.horas = hsProyectos;
     }
 };
+
+//Cargo proyectos del Json al presionar boton
+//Declaro la url donde tengo el archivo JSON local
+const URLJSONPROY = "./datos/proyectos.json";
+
+$.getJSON(URLJSONPROY, function(respuesta, estado) {
+    if (estado === "success") {
+        respuesta.map(p => proyectos.push(p));
+        mostrarProyectos();
+    } else {
+        console.log('Error al cargar proyectos')
+    }
+});
+
+//Cargo empleados del Json al presionar boton
+//Declaro la url donde tengo el archivo JSON local
+const URLJSON = "./datos/empleados.json";
+//Agrego botón con jQuery
+$("#empleados").append('<button id="btnJ" class="boton-cargar">Cargar</button>');
+//Escucho el evento click del botón json
+$("#btnJ").click(() => {
+    $.getJSON(URLJSON, function(respuesta, estado) {
+        if (estado === "success") {
+            empleados = respuesta;
+            $("#listaEmpleados").html("");
+            $("#listaEmpleados").append(`<div id="listaEmpleados" class="desarrolladores"><strong>Desarrolladores</strong>`);
+            for (let des of empleados) {
+                $("#listaEmpleados").append(`<div id="listaEmpleados" class="desarrolladores">${des.nombre}`);
+            }
+        }
+    })
+});
--- js/funciones.js
+++ js/funciones.js
@@ -1,35 +1,3 @@
-//Cargo proyectos del Json al presionar boton
-//Declaro la url donde tengo el archivo JSON local
-const URLJSONPROY = "./datos/proyectos.json";
-
-$.getJSON(URLJSONPROY, function(respuesta, estado) {
-    if (estado === "success") {
-        respuesta.map(p => proyectos.push(p));
-        mostrarProyectos();
-    } else {
-        console.log('Error al cargar proyectos')
-    }
-});
-
-//Cargo empleados del Json al presionar boton
-//Declaro la url donde tengo el archivo JSON local
-const URLJSON = "./datos/empleados.json";
-//Agrego botón con jQuery
-$("#empleados").append('<button id="btnJ" class="boton-cargar">Cargar</button>');
-//Escucho el evento click del botón json
-$("#btnJ").click(() => {
-    $.getJSON(URLJSON, function(respuesta, estado) {
-        if (estado === "success") {
-            empleados = respuesta;
-            $("#listaEmpleados").html("");
-            $("#listaEmpleados").append(`<div id="listaEmpleados" class="desarrolladores"><strong>Desarrolladores</strong>`);
-            for (let des of empleados) {
-                $("#listaEmpleados").append(`<div id="listaEmpleados" class="desarrolladores">${des.nombre}`);
-            }
-        }
-    })
-});
-
 //Función que reasigna los Id y pone en 0 las horas totales de proyecto y de empleados
 function reasignarId() {
     //recorro el array de proyectos
